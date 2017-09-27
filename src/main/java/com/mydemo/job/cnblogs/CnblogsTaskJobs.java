011db0405f7c1583a255ab338ed9e992754b4098
watermelon11 <1529923213@qq.com>
2017-09-27 16:54:50 +0800

    爬虫

--- src/main/java/com/mydemo/job/cnblogs/CnblogsTaskJobs.java
+++ src/main/java/com/mydemo/job/cnblogs/CnblogsTaskJobs.java
@@ -1,11 +1,15 @@
 package com.mydemo.job.cnblogs;
 
-import com.mydemo.common.Constant;
 import com.mydemo.domain.Article;
 import com.mydemo.service.ArticleService;
+import org.apache.http.Consts;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
+import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
@@ -19,32 +23,58 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Component
 public class CnblogsTaskJobs {
 
     private final static Logger logger = LoggerFactory.getLogger(CnblogsTaskJobs.class);
-    private static final String url = "https://www.cnblogs.com/cate/java/";
+    private final static String baseurl = "https://www.cnblogs.com/cate/java/";
+    private final static String pageurl = "https://www.cnblogs.com/mvc/AggSite/PostList.aspx";
+
+    private static Map<String,String> pageMap = new HashMap<>();
+    static{
+        pageMap.put("CategoryType","SiteCategory");
+        pageMap.put("ParentCategoryId", "2");
+        pageMap.put("CategoryId","106876");
+        pageMap.put("PageIndex","2");
+        pageMap.put("TotalPostCount","4000");
+        pageMap.put("ItemListActionName","PostList");
+    }
 
     @Resource
     private ArticleService articleService;
 
-    @Scheduled(cron = "0 40 13 * * ? ")
+    @Scheduled(cron = "0 15 16 * * ? ")
     public void pullOnce(){
         logger.info("开始搞事.............");
-        int pageNum = pull(url);
+        int pageNum = pull(baseurl);
         for(int i=2;i<=pageNum;i++){
-            pull(url+"#p"+i);
+            pageMap.put("PageIndex",String.valueOf(i));
+            pagePull(pageurl,pageMap);
         }
         logger.info("搞事结束.............");
     }
 
 
-//    @Scheduled(cron = "0 7 11 * * ? ")
-//    public void pullEveryDay(){
-//
-//    }
+    @Scheduled(cron = "0 0 3 * * ? ")
+    public void pullEveryDay(){
+        Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE,-3);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String date = simpleDateFormat.format(cal.getTime());
+        logger.info("开始搞事.............");
+        int pageNum = pull(baseurl);
+        for(int i=2;i<=pageNum;i++){
+            pageMap.put("PageIndex",String.valueOf(i));
+            pagePull(pageurl,pageMap,date);
+        }
+        logger.info("搞事结束.............");
+    }
 
 
     private int pull(String url){
@@ -56,32 +86,84 @@ public class CnblogsTaskJobs {
             Elements pages = page.child(0).children();
             pageNum = Integer.valueOf(pages.get(pages.size()-2).text());
             Elements elements = doc.getElementsByClass("post_item");
-            for(Element element : elements){
-                Elements viewElement = element.getElementsByClass("article_view");
-                String value = viewElement.get(0).child(0).text();
-                String regEx="[^0-9]";
-                value = value.replaceAll(regEx,"");
-                if(Integer.valueOf(value)>=100){
-                    Elements lnkElement = element.getElementsByClass("titlelnk");
-                    String href = lnkElement.attr("href");
-                    body = get(href);
-                    Document document = Jsoup.parseBodyFragment(body);
-                    Element articleEle = document.getElementById("cnblogs_post_body");
-                    Element title = document.getElementById("cb_post_title_url");
-                    Article article = new Article();
-                    article.setContent(articleEle.toString());
-                    article.setTitle(title.text());
-                    article.setCategoryId(2l);
-                    article.setUserId(1l);
-                    article.setViewCount(0);
-                    article.setCommentCount(0);
-                    articleService.addArticle(article);
+            save(elements);
+        } catch (Exception e) {
+            logger.info(e.getMessage(), e);
+        }
+        return pageNum;
+    }
+
+
+    private void save(Elements elements) throws Exception{
+        for(Element element : elements){
+            Elements viewElement = element.getElementsByClass("article_view");
+            String value = viewElement.get(0).child(0).text();
+            String regEx="[^0-9]";
+            value = value.replaceAll(regEx,"");
+            if(Integer.valueOf(value)>=100){
+                Elements lnkElement = element.getElementsByClass("titlelnk");
+                String href = lnkElement.attr("href");
+                String body = get(href);
+                Document document = Jsoup.parseBodyFragment(body);
+                Element articleEle = document.getElementById("cnblogs_post_body");
+                Element title = document.getElementById("cb_post_title_url");
+                Article article = new Article();
+                article.setContent(articleEle.toString());
+                article.setTitle(title.text());
+                article.setCategoryId(2l);
+                article.setUserId(1l);
+                article.setViewCount(0);
+                article.setCommentCount(0);
+                articleService.addArticle(article);
+            }
+        }
+    }
+
+    public void pagePull(String pageUrl,Map<String,String> dataMap){
+        try {
+            String body = post(pageUrl,dataMap);
+            Document doc = Jsoup.parseBodyFragment(body);
+            Elements elements = doc.getElementsByClass("post_item");
+            save(elements);
+        } catch (Exception e) {
+            logger.info(e.getMessage(), e);
+        }
+    }
+
+    public void pagePull(String pageUrl,Map<String,String> dataMap,String yesDate){
+        try {
+            String body = post(pageUrl,dataMap);
+            Document doc = Jsoup.parseBodyFragment(body);
+            Elements elements = doc.getElementsByClass("post_item");
+            for(Element element : elements) {
+                Elements foot = element.getElementsByClass("post_item_foot");
+                String pullDate = foot.get(0).child(0).text().substring(4);
+                if (pullDate.compareTo(yesDate)>0) {
+                    Elements viewElement = element.getElementsByClass("article_view");
+                    String value = viewElement.get(0).child(0).text();
+                    String regEx = "[^0-9]";
+                    value = value.replaceAll(regEx, "");
+                    if (Integer.valueOf(value) >= 100) {
+                        Elements lnkElement = element.getElementsByClass("titlelnk");
+                        String href = lnkElement.attr("href");
+                        body = get(href);
+                        Document document = Jsoup.parseBodyFragment(body);
+                        Element articleEle = document.getElementById("cnblogs_post_body");
+                        Element title = document.getElementById("cb_post_title_url");
+                        Article article = new Article();
+                        article.setContent(articleEle.toString());
+                        article.setTitle(title.text());
+                        article.setCategoryId(2l);
+                        article.setUserId(1l);
+                        article.setViewCount(0);
+                        article.setCommentCount(0);
+                        articleService.addArticle(article);
+                    }
                 }
             }
         } catch (Exception e) {
             logger.info(e.getMessage(), e);
         }
-        return pageNum;
     }
 
     private String get(String url) throws Exception{
@@ -89,6 +171,24 @@ public class CnblogsTaskJobs {
         CloseableHttpClient closeableHttpClient = HttpClientBuilder.create().build();
         HttpGet httpGet = new HttpGet(url);
         CloseableHttpResponse response = closeableHttpClient.execute(httpGet);
+        HttpEntity httpEntity = response.getEntity();
+        if (httpEntity != null) {
+            body = EntityUtils.toString(httpEntity, "UTF-8");
+        }
+        closeableHttpClient.close();
+        return body;
+    }
+
+    private String post(String url,Map<String,String> data)throws Exception{
+        String body = null;
+        CloseableHttpClient closeableHttpClient = HttpClientBuilder.create().build();
+        HttpPost httpPost = new HttpPost(url);
+        MultipartEntityBuilder multipartEntity = MultipartEntityBuilder.create();
+        data.forEach((k,v)->{
+            multipartEntity.addPart(k, new StringBody(v, ContentType.create("text/plain", Consts.UTF_8)));
+        });
+        httpPost.setEntity(multipartEntity.build());
+        CloseableHttpResponse response = closeableHttpClient.execute(httpPost);
         HttpEntity httpEntity = response.getEntity();
         if (httpEntity != null) {
             body = EntityUtils.toString(httpEntity, "UTF-8");
